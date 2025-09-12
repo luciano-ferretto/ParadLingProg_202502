4ab58312d099507fa15820c3b8498038b243230d
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2025-09-12 19:47:10 -0300

    Atividade avaliativa

--- microservices/greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
+++ microservices/greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
@@ -1,13 +1,19 @@
 package br.edu.atitus.greeting_service.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.atitus.greeting_service.configs.GreetingConfig;
+import br.edu.atitus.greeting_service.dtos.GreetingDTO;
 
 @RestController
 @RequestMapping("greeting")
@@ -24,15 +30,48 @@ public class GreetingController {
 		super();
 		this.config = config;
 	}
-
-	@GetMapping
+	
+	
+	@GetMapping({"", "/{namePath}"})
 	public ResponseEntity<String> greet(
-			@RequestParam(required = false) String name){
+			@RequestParam(required = false) String name,
+			@PathVariable(required = false) String namePath){
 		String greetingReturn = config.getGreeting();
-		String nameReturn = name != null ? name : config.getDefaultName();
+		String nameReturn = name != null 
+				? name 
+				: namePath != null ? namePath : config.getDefaultName();
+		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
+		
+		return ResponseEntity.ok(textReturn);
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> greetPost(
+			@RequestBody GreetingDTO dto){
+		String greetingReturn = config.getGreeting();
+		String nameReturn = dto.name(); 
 		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
 		
 		return ResponseEntity.ok(textReturn);
 	}
 
+	
+//	@GetMapping("/{namePath}")
+//	public ResponseEntity<String> greetPaht(
+//			@PathVariable String namePath){
+//		return greet(namePath);
+//	}
+//
+//	@GetMapping
+//	public ResponseEntity<String> greet(
+//			@RequestParam(required = false) String name){
+//		String greetingReturn = config.getGreeting();
+//		String nameReturn = name != null ? name : config.getDefaultName();
+//		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
+//		
+//		return ResponseEntity.ok(textReturn);
+//	}
+
+	
+	
 }
